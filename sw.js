da5d969f036c343dd4d80e346780029a68a2bdad
delgadoescobarmanuel-pixel <delgadoescobarmanuel@gmail.com>
2026-08-31 18:38:18 +0200

    feat: inject hold-to-pause teleprompter control

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='teleprompter-recorder-v3';
+const CACHE='teleprompter-recorder-v4';
 self.addEventListener('install',e=>{
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./cloud.js','./recorder-fix.js'])).catch(()=>{}));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./cloud.js','./recorder-fix.js','./hold-scroll.js'])).catch(()=>{}));
 });
 self.addEventListener('activate',e=>{
   e.waitUntil(Promise.all([
@@ -14,6 +14,7 @@ self.addEventListener('fetch',e=>{
     e.respondWith(fetch(e.request,{cache:'no-store'}).then(async r=>{
       let html=await r.text();
       if(!html.includes('recorder-fix.js')) html=html.replace('</body>','<script src="./recorder-fix.js?v=3"></script></body>');
+      if(!html.includes('hold-scroll.js')) html=html.replace('</body>','<script src="./hold-scroll.js?v=1"></script></body>');
       if(!html.includes('cloud.js')) html=html.replace('</body>','<script src="./cloud.js?v=1"></script></body>');
       return new Response(html,{status:r.status,statusText:r.statusText,headers:{'Content-Type':'text/html; charset=utf-8','Cache-Control':'no-store'}});
     }).catch(()=>caches.match(e.request)));
